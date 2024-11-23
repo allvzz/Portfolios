edc321253cd78f6c57763fd03f0dfdcb6f70bd7d
Alvaro <a.antoonn0@gmail.com>
2024-11-23 00:23:11 -0300

    Add files via upload
    Projeto privado para fins academicos, somente exposto para quem for visualizar ter uma noção básica do que se trata. Favor não copiar o código, pois o mesmo tem direitos autorais nele.

---
+++ Protect Max/assets/js/script.js
@@ -0,0 +1,5 @@
+// Função para rolar até a seção de produtos
+function scrollToProdutos() {
+    const produtosSection = document.getElementById('produtos');
+    produtosSection.scrollIntoView({ behavior: 'smooth' });
+}
